58034265698ace17f9f8997d2cd6c9df37321c4b
Kenyibrayan <brayanhuallpa_15@hotmail.com>
2022-02-09 20:37:24 -0500

    terminado

--- mainforma1.js
+++ mainforma1.js
@@ -15,10 +15,47 @@ const tarea = {
      let obtener = JSON.parse(localStorage.getItem("tareas"));
      obtener.push(tarea);
      localStorage.setItem("tareas", JSON.stringify(obtener));
+
  }
+ mostrarTareas();
  input.value = "";
 
 };
+
+const mostrarTareas = () => {
+    let tareas_obtenidas = JSON.parse (localStorage.getItem("tareas"));
+    container_todo.innerHTML = ""; '
+
+    for (let i = 0; i < tareas_obtenidas.length; i++) {
+let input = tareas_obtenidas[i].input_tarea;
+container_todo.innerHTML +=
+<div class= "container_list">
+    <div class="container_list-1">
+        <input type="checkbox" class="casilla">
+            <p class="actividad"> $ {input} </p>
+         </div>
+        <div class="container_list-btn">
+            <button class="btn-eliminar"><i class = "fas fa-trash-alt"></i></button>   
+         </div>
+        </div>
+        
+      }
+}
+
+const eliminarTarea = (tarea) => {
+    let tareas = JSON.parse(localStorage.getItem("tareas"));
+    for (let i = 0; i <tareas.length; i++ {
+        if(tarea ≡ tareas[i].input_tarea){
+            tarea.splice(i,1);
+        }
+    } 
+    localStorage.setItem("tareas", JSON.stringify(tareas));
+    mostrarTareas();
+
+}
+
+
+
 btn_todo.addEventListener("click",  ()  {
     if(input.value ≡  "" input.value.trim () "") {
         window.alert("Imput vacio, ingreso datos");
@@ -26,5 +63,7 @@ btn_todo.addEventListener("click",  ()  {
         guardarTareas();
 
     }
+    input.value = "";
+
 
 });
